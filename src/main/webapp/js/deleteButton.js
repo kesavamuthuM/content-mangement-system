d00b3c3e57583a94ecacef0d45825d806608061e
kesavamuthuM <kesav.muthuraj@anywhere.co>
2019-04-05 16:26:56 +0530

    Separating css from html into seperate file

--- src/main/webapp/js/deleteButton.js
+++ src/main/webapp/js/deleteButton.js
@@ -2,11 +2,31 @@
  * 
  */
 
-$(document).ready(function(){
-			$(".linkBtn").mouseenter(function(){
-				$(this).append(" <span> <a id=\"hai\" href=\"#\" class=\"glyphicon glyphicon-trash \" style=\"float : right;\"></a></span>");
-				$(".linkBtn").mouseleave(function(){
-					$("#hai").remove();
-				})
-			})
-		});
+$(document)
+		.ready(
+				function() {
+					var i = 4;
+
+					$("#submit")
+							.click(
+									function() {
+										$("#contactsId")
+												.append(
+														"<a href=\"#\" class=\"btn btn-default btn-block linkBtn\">contact 8 </a>	");
+
+									});
+					$(".subJumbo")
+							.on(
+									"mouseenter",
+									".linkBtn",
+									function() {
+										$(this)
+												.append(
+														" <span> <a id=\"hai\" href=\"#\" class=\"glyphicon glyphicon-trash \" style=\"float : right;\"></a></span>");
+
+										$(".linkBtn").mouseleave(function() {
+											$("#hai").remove();
+
+										})
+									});
+				});
