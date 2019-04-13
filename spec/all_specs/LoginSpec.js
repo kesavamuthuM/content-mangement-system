e2e7b337674fa9331f3e2ef8d131aeff22594b56
kesavamuthuM <kesav.muthuraj@anywhere.co>
2019-04-13 22:17:06 +0530

    have some changes in js file

--- spec/all_specs/LoginSpec.js
+++ spec/all_specs/LoginSpec.js
@@ -3,8 +3,8 @@
  */
 
 describe("check the entered input works", function() {
-	var Login = require('../../src/main/webapp/js/LoginHandler.js');
+	var Login = require('../../src/main/webapp/js/Login.js');
 	it("check whether works or not", function() {
-		var loginhandler = new Login();
+		
 	})
 })
